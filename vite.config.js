fb423c4adb5eeed725fa176ef202980a53dea368
Edmund Dao <61922101+64blit@users.noreply.github.com>
2024-05-09 21:06:33 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import viteReact from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-const { readFileSync, writeFileSync } = require('fs')
-const { join } = require('path')
 
 const path = fileURLToPath(import.meta.url)
 const root = resolve(dirname(path), 'client')
